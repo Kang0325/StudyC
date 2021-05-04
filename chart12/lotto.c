4a5621041111430936b08414cd32a506b9a6f27e
Kang0325 <noworry1@naver.com>
2021-05-04 09:04:41 +0900

    210504 lotto

--- chart12/lotto.c
+++ chart12/lotto.c
@@ -23,26 +23,18 @@ void input_lotto(int* lt)
 	int i, j;
 	for (i = 0; i < 6; i++)
 	{
-		if (*(lt + i) < 46)
+		printf("%d 번째 ", i + 1);
+		printf("로또 번호 입력 : ");
+		scanf("%d", lt + i);				// for문이므로 lt 값에도 +i를 해줘야함
+		
+		for (j = 0; j < i; j++)
 		{
-			printf("%d 번째 ", i + 1);
-			printf("로또 번호 입력 : ");
-			scanf("%d", lt + i);				// for문이므로 lt 값에도 +i를 해줘야함
-
-			for (j = 0; j < i; j++)
+			if (*(lt + i) == *(lt + j))
 			{
-				if (*(lt + i) == *(lt + j))
-				{
-					printf("번호가 중복 되었습니다.\n");
-					i--;
-					break;
-				}
+				printf("번호가 중복 되었습니다.\n");
+				i--;
+				break;
 			}
-		}
-		else
-		{
-			printf("45 이하의 수를 입력하세요.\n");
-			i--;
 		}
 	}
 }
